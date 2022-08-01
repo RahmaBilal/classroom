f9ad640d506e0bf442f8c93bf9fec5484f3ae5db
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-08-01 19:31:23 +0100

    Classroom wk6 changes

--- Week5-Day1/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ Week5-Day1/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -2,5 +2,14 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
-
+const handleForm = (event) => {
+    event.preventDefault()
+    var inputValue = $('input[name="shopping-input"]').val();
+    console.log(inputValue);
+    var li = $("<li>");
+    li.text(inputValue);
+    shoppingListEl.append(li);
+    //shoppingFormEl.on("submit", handleForm);
+}
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFromEl.on("submit", handleForm);
